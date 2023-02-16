28ee2d185043a5f35cb421ea75c46d55b333838a
Alex <123321081+Goofball554@users.noreply.github.com>
2023-02-16 12:46:16 -0500

    Add files via upload

---
+++ Area of Circle.py
@@ -0,0 +1,7 @@
+import math
+def areaOfCircle(r):
+    # your code here
+    area = math.pi*(r**2)
+    return area
+
+print(areaOfCircle(10))
---
+++ SumTo.py
@@ -0,0 +1,6 @@
+def sumTo(n):
+    # your code here
+    sum = (n*(n+1))/2
+    return sum
+    
+print(sumTo(10))
